f140b3ffe3b445554e826c02ea46981661d05415
Alan Pita <pitaman512@gmail.com>
2023-11-27 23:41:57 -0600

    - [x] add time of day to Person.HasAvailability

--- typescript4/src/Domains/Logistics.ts
+++ typescript4/src/Domains/Logistics.ts
@@ -69,25 +69,128 @@ export const RadiusDomain = new class _EventDomain extends Elevated.Domain<Base.
     }
 };
 
+export interface _TimeOfDay {
+    hours?: number,
+    minutes?: number
+};
+
+export const TimeOfDayDomain = new class _TimeOfDayDomain extends Elevated.Domain<Base.Parseable & _TimeOfDay> {
+    asString(format?: string) { 
+        return {
+            from(text: string): Base.Parseable & _TimeOfDay { 
+                const match = new RegExp(/(\d+):(\d+)(am|pm|AM|PM)?/).exec(text);
+                if(!match) {
+                    return {
+                        text,
+                        error: 'Format error. Expected ##:##(am|pm}'
+                    }
+                } else {
+                    try {
+                        let hours = Number.parseInt(match[1]);
+                        let minutes = Number.parseInt(match[2]);
+                        let error: undefined|string;
+                        if(match[3] === 'am' || match[3] === 'AM') {
+                            if(hours === 12) hours = 0;
+                        } else if(match[3] === 'pm' || match[3] === 'PM') {
+                            if(hours !== 12) hours += 12;
+                        } 
+                        if(hours < 0 || hours > 23) {
+                            error = `Format error: expected hours (${hours}) to be in the range 0-23 or 1-12[am|pm]`;
+                        } else if(minutes < 0 || minutes > 59) {
+                            error = `Format error: expected minutes (${minutes}) to be in the range 0-59`;
+                        }
+                        return {
+                            text, error, hours, minutes
+                        }
+                    } catch(e) {
+                        return {
+                            text,
+                            error: 'Format error. Expected ##:##(am|pm}'
+                        }
+                    }
+                }                            
+            },
+            to(value: Base.Parseable & _TimeOfDayDomain) { return value.text || "" }
+        };
+    }
+    asEnumeration(maxCount: number) { return undefined }
+    asColumns() { return undefined }
+    cmp(a: Base.Parseable & _TimeOfDay, b: Base.Parseable & _TimeOfDay) {
+        if(a.hours === undefined || b.hours === undefined) {
+            if(a.hours === undefined && b.hours !== undefined) return -1;
+            if(a.hours !== undefined && b.hours === undefined) return 1;
+            return 0;
+        }
+        if(a.hours < b.hours) return -1;
+        if(a.hours > b.hours) return 1;
+        if(a.minutes === undefined || b.minutes === undefined) {
+            if(a.minutes === undefined && b.minutes !== undefined) return -1;
+            if(a.minutes !== undefined && b.minutes === undefined) return 1;
+            return 0;
+        }
+        if(a.minutes < b.minutes) return -1;
+        if(a.minutes > b.minutes) return 1;
+        return 0;
+    }
+};
+
+export interface _Date {
+    iso8601: string
+};
+
+export const DateDomain = new class _DateDomain extends Elevated.Domain<Base.Parseable & _Date> {
+    asString(format?: string) { 
+        return {
+            from(text: string): Base.Parseable & _Date { 
+                const asDate = new Date(text);
+                return { text: asDate.toLocaleDateString(), iso8601: asDate.toISOString() }
+            },
+            to(value: Base.Parseable & _Date) { return value.text || new Date(value.iso8601).toLocaleDateString() }
+        };
+    }
+    asEnumeration(maxCount: number) { return undefined }
+    asColumns() { return undefined }
+    cmp(a: Base.Parseable & _Date, b: Base.Parseable & _Date) {
+        if(a.iso8601 < b.iso8601)  return -1;
+        if(a.iso8601 > b.iso8601)  return 1;
+        return 0;
+    }
+};
+
 export interface _When {
-    before?: { iso8601: string };
-    after?: { iso8601: string };
-    at?: { iso8601: string };
-    from?: { iso8601: string };
-    to?: { iso8601: string };
-}
+    at?: _Date;
+    dates?: {
+        from: _Date & Base.Parseable;
+        to: _Date & Base.Parseable;
+    }, times?: { 
+        from: _TimeOfDay & Base.Parseable, 
+        to: _TimeOfDay & Base.Parseable
+    }
+};
 
 export const WhenDomain = new class _EventDomain extends Elevated.Domain<Base.Parseable & _When> {
-    asRange() {
+    asTimes() {
         return {
-            from(text_: { from: string, to: string }): Base.Parseable & _When { 
-                const from = { iso8601: new Date(text_.from).toISOString() };
-                const to = { iso8601: new Date(text_.to).toISOString() };
-                const text = `${new Date(from.iso8601).toLocaleDateString()} - ${new Date(to.iso8601).toLocaleDateString()}`;
-                const error = new Date(from.iso8601) <= new Date(to.iso8601) ? undefined : `Date range: 'from' date must occur before 'to' date`;
-                return { text, error, from, to };
+            from(text_: { from: string, to: string }, basis: _When = {}): Base.Parseable & _When {            
+                const from = TimeOfDayDomain.asString().from(text_.from);
+                const to = TimeOfDayDomain.asString().from(text_.to);
+                const text = `${text_.from} - ${text_.to}` + (basis?.dates === undefined ? '' : ` ${basis.dates.from.text} - ${basis.dates.to.text}`);
+                const error = TimeOfDayDomain.cmp(from, to) <= 0 ? undefined : `WhenDomain.asTimes: 'from' time must occur before 'to' time`;
+                return { ...basis, text, error, times: { from, to } };
             },
-            to(value: Base.Parseable & _When) { return { from: value.from?.iso8601, to: value.to?.iso8601 } }
+            to(value: Base.Parseable & _When) { return { from: value.times?.from.text, to: value.times?.to.text } }
+        };
+    }
+    asDates() {
+        return {
+            from(text_: { from: string, to: string }, basis: _When = {}): Base.Parseable & _When { 
+                const from = DateDomain.asString().from(text_.from);
+                const to = DateDomain.asString().from(text_.to);
+                const text = (basis?.times === undefined ? '' : `${basis.times.from.text} - ${basis.times.to.text} `) + `${text_.from} - ${text_.to}`;
+                const error = new Date(from.iso8601) <= new Date(to.iso8601) ? undefined : `WhenDomain.asDates: 'from' date must occur before 'to' date`;
+                return { ...basis, text, error, dates: { from, to } };
+            },
+            to(value: Base.Parseable & _When) { return { from: value.dates?.from.iso8601, to: value.dates?.to.iso8601 } }
         };
     }
     asString(format?: string) { 
